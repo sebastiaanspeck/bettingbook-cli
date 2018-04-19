6ad3cea7a042ad71dd9b929ede868ec15eebe912
Sebastiaan Speck <1420194speck@zuyd.nl>
2018-04-19 17:32:36 +0200

    Added comments to leagueids.py
    Added comments to leagueids.py to see better which league it is

--- bettingbook/leagueids.py
+++ bettingbook/leagueids.py
@@ -1,9 +1,16 @@
 LEAGUE_IDS = {
+    # Premier League - England
     "GB1": 8,
+    # Eredivisie - Netherlands
     "NL1": 72,
+    # Bundesliga - Germany
     "DE1": 82,
+    # Jupiler Pro League - Belgium
     "BE1": 208,
+    # Ligue 1 - France
     "FR1": 301,
+    # Serie A - Italy
     "IT1": 384,
+    # Primera Division - Spain
     "ES1": 564
 }
