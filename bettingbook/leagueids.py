751897f1affa5a5e94c92be34e1fb6576ddd5c2b
Sebastiaan Speck <1420194speck@zuyd.nl>
2018-04-20 14:18:52 +0200

    Updated leagues
    Updated leagues with more data

--- bettingbook/leagueids.py
+++ bettingbook/leagueids.py
@@ -1,16 +1,16 @@
 LEAGUE_IDS = {
     # Premier League - England
-    "GB1": [8],
+    "EN1": [8],
     # Eredivisie - Netherlands
     "NL1": [72, 1353],
     # Bundesliga - Germany
     "DE1": [82, 163, 166],
-    # Jupiler Pro League - Belgium
-    "BE1": [208, 217],
+    # Pro League - Belgium
+    "BE1": [208, 217, 1371],
     # Ligue 1 - France
     "FR1": [301],
     # Serie A - Italy
     "IT1": [384],
-    # Primera Division - Spain
+    # La Liga - Spain
     "ES1": [564]
 }
