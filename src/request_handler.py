5e1502c6cae12a9f5721552bca3de3dad457b8ff
Sebastiaan Speck <shem.speck@gmail.com>
2021-10-16 20:24:50 +0200

    Added some functionality to get_multi_matches

--- src/request_handler.py
+++ src/request_handler.py
@@ -165,6 +165,11 @@ class RequestHandler(object):
         while True:
             self.writer.league_scores(self._get(f"fixtures/multi/{match_ids}"), parameters, True, predictions)
             time.sleep(60)
+        fixtures = self._get(f"fixtures/multi/{match_ids}")
+        if len(fixtures) == 0:
+            click.secho(parameters.msg[0], fg="red", bold=True)
+            return
+        self.writer.league_scores(self._get(f"fixtures/multi/{match_ids}"), parameters, True, predictions)   
 
     def get_match_data(self, parameters, start, end, first=False):
         if parameters.type_sort == "matches":
